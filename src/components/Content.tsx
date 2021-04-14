bb490a30f072d3b341e9a84b80f9e62b1820c396
Augusto <augustomsaluz@gmail.com>
2021-04-13 21:54:30 -0300

    divided components SideBar and Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,61 @@
-export function Content() {
+import { useEffect, useState } from 'react';
+
+import { MovieCard } from '../components/MovieCard';
+
+import { api } from '../services/api';
+
+import './styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface SideBarProps {
+  onSelectedGenre: (genre: number) => void;
+  isSelected: number;
+}
+
+export function Content({ isSelected, onSelectedGenre }: SideBarProps) {
   // Complete aqui
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${isSelected}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${isSelected}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [isSelected]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
